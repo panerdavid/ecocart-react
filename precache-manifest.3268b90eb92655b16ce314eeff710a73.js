d1e5130b090f06195a6fc04b9d5062a6c5b2681e
David Paner <panerdavid@Davids-MacBook-Pro.local>
2020-08-03 19:55:55 -0700

    Updates

---
+++ precache-manifest.3268b90eb92655b16ce314eeff710a73.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cebaf7872fb6f224cd51736395047a1e",
+    "url": "/ecocart-react/index.html"
+  },
+  {
+    "revision": "1d8bf8c91fdcc86b077b",
+    "url": "/ecocart-react/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "884134d9f710f031e452",
+    "url": "/ecocart-react/static/css/main.19f94e63.chunk.css"
+  },
+  {
+    "revision": "1d8bf8c91fdcc86b077b",
+    "url": "/ecocart-react/static/js/2.95604276.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/ecocart-react/static/js/2.95604276.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "884134d9f710f031e452",
+    "url": "/ecocart-react/static/js/main.b3e2b9a0.chunk.js"
+  },
+  {
+    "revision": "5fd70b3020f08a57531e",
+    "url": "/ecocart-react/static/js/runtime-main.0b6edd26.js"
+  },
+  {
+    "revision": "1031e29c7bb6f4c93460422f0130a1f9",
+    "url": "/ecocart-react/static/media/hero.1031e29c.png"
+  },
+  {
+    "revision": "a435fd687a9a5a05c7911c2e71359da6",
+    "url": "/ecocart-react/static/media/logo.a435fd68.png"
+  }
+]);
