984bd616851deb0c0bc42078e12e4b7821ababd8
David Paner <panerdavid@Davids-MacBook-Pro.local>
2020-08-07 07:04:58 -0700

    Updates

---
+++ precache-manifest.084edb6569c41571c8f4f5f4c301beff.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ea7a74d04ae28d51d2ba6714da364ec",
+    "url": "/ecocart-react/index.html"
+  },
+  {
+    "revision": "041df0533e02b1942c0c",
+    "url": "/ecocart-react/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "cf5000e230ac23f8d953",
+    "url": "/ecocart-react/static/css/main.eae728ef.chunk.css"
+  },
+  {
+    "revision": "041df0533e02b1942c0c",
+    "url": "/ecocart-react/static/js/2.b3dec341.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/ecocart-react/static/js/2.b3dec341.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf5000e230ac23f8d953",
+    "url": "/ecocart-react/static/js/main.b7bbbe41.chunk.js"
+  },
+  {
+    "revision": "5fd70b3020f08a57531e",
+    "url": "/ecocart-react/static/js/runtime-main.0b6edd26.js"
+  },
+  {
+    "revision": "c0f2f644943a87a6bfbafbe97b80e199",
+    "url": "/ecocart-react/static/media/UN-Sustainability-Goal-13.c0f2f644.png"
+  },
+  {
+    "revision": "7c2299b6d3af24abcf1c6db08c9108c1",
+    "url": "/ecocart-react/static/media/UN-Sustainability-Goal-14.7c2299b6.png"
+  },
+  {
+    "revision": "ecc0cbba849a2634beab9b59fef25b54",
+    "url": "/ecocart-react/static/media/UN-Sustainability-Goal-15.ecc0cbba.png"
+  },
+  {
+    "revision": "bc714a7011ce3942d46910e79dbda7bb",
+    "url": "/ecocart-react/static/media/UN-Sustainability-Goal-6.bc714a70.png"
+  },
+  {
+    "revision": "a402b31857b37e1a2f717fdaffe2116a",
+    "url": "/ecocart-react/static/media/UN-Sustainability-Goal-8.a402b318.png"
+  },
+  {
+    "revision": "6680f5f7a8471c86393547a3010fdf3f",
+    "url": "/ecocart-react/static/media/american-carbon-registry.6680f5f7.jpg"
+  },
+  {
+    "revision": "d17a0e8bb65e2f2b764bdc19201a3e2d",
+    "url": "/ecocart-react/static/media/earth.d17a0e8b.png"
+  },
+  {
+    "revision": "5452cdd051fc99a4335bf69f1da6b20f",
+    "url": "/ecocart-react/static/media/forest-project.5452cdd0.png"
+  },
+  {
+    "revision": "99b0e4e73bad882238b9accef2ef7435",
+    "url": "/ecocart-react/static/media/gold-standard.99b0e4e7.png"
+  },
+  {
+    "revision": "1031e29c7bb6f4c93460422f0130a1f9",
+    "url": "/ecocart-react/static/media/hero.1031e29c.png"
+  },
+  {
+    "revision": "a435fd687a9a5a05c7911c2e71359da6",
+    "url": "/ecocart-react/static/media/logo.a435fd68.png"
+  },
+  {
+    "revision": "e2b2395a1dc832a64325094859660b3e",
+    "url": "/ecocart-react/static/media/tree.e2b2395a.png"
+  },
+  {
+    "revision": "d4e88a39b0f04a9fd0794099d2c31c35",
+    "url": "/ecocart-react/static/media/verified-carbon-offset.d4e88a39.png"
+  },
+  {
+    "revision": "00d9dfb211c1f00f0cc3dcd8bab0f2ef",
+    "url": "/ecocart-react/static/media/water-project.00d9dfb2.png"
+  },
+  {
+    "revision": "eccc4046a83b4ffe29151d4ab1716e47",
+    "url": "/ecocart-react/static/media/wind-project.eccc4046.png"
+  },
+  {
+    "revision": "3d00d7615394f4a53661d7ae1924af2b",
+    "url": "/ecocart-react/static/media/works.3d00d761.mp4"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ecocart-react/precache-manifest.4ac3d1d25c41a93b55c3f6ac293b439e.js"
+  "/ecocart-react/precache-manifest.084edb6569c41571c8f4f5f4c301beff.js"
 );
 
 self.addEventListener('message', (event) => {
